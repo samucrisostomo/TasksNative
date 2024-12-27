0228ecd36876ffa6a2b64c22b5b89350662ef4a6
samuel <samolivercris@gmail.com>
2024-12-27 20:52:05 -0300

    Importando Tailwind manualmente

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Ajuste o caminho conforme seu projeto
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
